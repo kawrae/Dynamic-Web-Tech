b9ab07a28a6aaf3f6819591a5eb96966418ed5bc
Corey Black <coreyblack@hotmail.co.uk>
2026-01-26 18:05:54 +0000

    Lab 2 setup

---
+++ Lab2/lab-2-app/src/App.jsx
@@ -0,0 +1,11 @@
+import Profile from "./Profile";
+import Gallery from "./Gallery";
+
+export default function App() {
+  return (
+    <div>
+      <Profile />
+      <Gallery />
+    </div>
+  );
+}
