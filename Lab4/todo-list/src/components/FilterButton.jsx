c99b485be697e405627bfdc95e6105b1ac9c5365
Corey Black <coreyblack@hotmail.co.uk>
2026-02-10 14:54:27 +0000

    Update FilterButton.jsx

--- Lab4/todo-list/src/components/FilterButton.jsx
+++ Lab4/todo-list/src/components/FilterButton.jsx
@@ -7,7 +7,7 @@ function FilterButton({ name, isPressed, setFilter }) {
       onClick={() => setFilter(name)}
     >
       <span className="visually-hidden">Show </span>
-      <span>{name} </span>
+      <span>{name}</span>
       <span className="visually-hidden"> tasks</span>
     </button>
   );
